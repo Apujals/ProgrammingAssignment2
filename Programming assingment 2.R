f744761b359e308148199ac8200a19a633b9486c
Apujals <apujals13@gmail.com>
2017-04-10 17:53:49 +0200

    Update Programming assingment 2.R

--- Programming assingment 2.R
+++ Programming assingment 2.R
@@ -1,26 +1,50 @@
+## first, for making quicklier the test, create a simple matrix 2x2
+x <- matrix(1:4, 2,  2)
+
+
 makeCacheMatrix <- function(x = matrix()) {
+
+  ## nothing is cached so set it to NULL
   inv <- NULL
+  
+  ## storing the matrix
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
+  
+  ## return the matrix
   get <- function() x
-  setInv <- function(solve) inv <<- solve
-  getInv <- function() inv
+  
+  ## caching  
+  setInverse <- function(inverse) inv <<- inverse
+  
+  ## getting the cached value
+  getInverse <- function() inv
+  
+  ## list of the functions "names = functions"
   list(set = set,
        get = get,
-       setInv = setInv,
-       getInv = getInv)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-cacheSolve <- function(x = matrix(), ...) {
-  inv <- x$getInv()
-  if(!is.null(inv)) {
-    message("getting cached inverse matrix")
+cacheSolve <- function(A, ...) {
+ 
+  ## gets the matrix and store it in "inv"
+  inv <- A$getInverse()
+  
+  #if found --> not NULL; and return it
+  if (!is.null(inv)) {
+    message("getting cached data")
     return(inv)
   }
-  data <- x$get
-  inv <- solve(data)
-  x$setInv(inv)
+  
+  ## not found --> createing inverse matrix with "solve" function
+  mat <- A$get()
+  inv <- solve(mat, ...)
+  
+  ## returning inverse matrix
+  A$setInverse(inv)
   inv
 }
